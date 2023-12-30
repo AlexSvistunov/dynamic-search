b0d26503a5d3414d2d4c53a16edcc478db0a2cfb
Alexey Svistunov <xdayx53@gmail.com>
2023-12-30 10:35:55 +0300

    added modules + added sort function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const list = document.querySelector('.list');
-const input = document.querySelector('.input');
-let workArray;
 
+let serverArray;
+import {list, input, sortBtn} from "./variables.js";
 
 async function responseApi() {
   const response = await fetch(`https://gist.githubusercontent.com/VasilyMur/43ef6df83bba694f871f11a16ed7556d/raw/b6edff674e35452d6c57ec64177a558f7adb432e/moscowSubway.json`);
   const normalResponse = await response.json(response);
+  console.log(normalResponse);
   const array = normalResponse.map(el => {
     return el.name;
   });
@@ -16,7 +16,7 @@ async function responseApi() {
 
 async function dataToListHtml() {
   const arrayOfData = await responseApi();
-  workArray = arrayOfData;
+  serverArray = arrayOfData;
   arrayOfData.forEach(data => {
     const li = document.createElement('li');
     li.classList.add('list__item');
@@ -24,6 +24,8 @@ async function dataToListHtml() {
     list.append(li);
   });
 }
+
+
 
 function updateList(array) {
   const list = document.querySelector('.list');
@@ -43,18 +45,39 @@ dataToListHtml();
 input.addEventListener('input', (e) => {
   const list = document.querySelector('.list');
   list.innerHTML = '';
-  let filteredArray = workArray;
+  let filteredArray = serverArray;
   const inputValue = e.currentTarget.value.toLowerCase();
-  for (let i = 0; i < workArray.length; i++) {
-    if(workArray[i].toLowerCase().startsWith(inputValue)) {
-      filteredArray = workArray.filter(el => el === workArray[i]);
+  for (let i = 0; i < serverArray.length; i++) {
+    if(serverArray[i].toLowerCase().startsWith(inputValue)) {
+      filteredArray = serverArray.filter(el => el === serverArray[i]);
       updateList(filteredArray)
     }
   }
 
-
   
 });
+
+
+function filterArray(array) {
+  return array.sort((a, b) => a.localeCompare(b));
+}
+
+
+sortBtn.addEventListener('click', () => {
+  const list = document.querySelector('.list');
+  list.innerHTML = '';
+  serverArray = filterArray(serverArray)
+  updateList(serverArray);
+
+});
+
+
+//сортировка по кол-во станций
+//автодополнение(высвечивается штука, нажимаешь и все)
+
+//вы до этого искали
+//подсветка результатов
+//загрузка
 
 
 
---
+++ variables.js
@@ -0,0 +1,3 @@
+export const list = document.querySelector('.list');
+export const input = document.querySelector('.input');
+export const sortBtn = document.querySelector('.sort');
